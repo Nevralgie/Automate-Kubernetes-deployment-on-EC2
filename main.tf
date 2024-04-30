b9f19dc658560afeeb3b4d974001c5301f5b8d2c
Tom Renard <tomrenard69@gmail.com>
2024-04-30 13:39:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
     address        = "https://gitlab.com/ara1504621/terraform-test/terraform/state/ppro_state"
     lock_address   = "https://gitlab.com/ara1504621/terraform-test/terraform/state/ppro_state/lock"
     unlock_address = "https://gitlab.com/ara1504621/terraform-test/terraform/state/ppro_state/lock"
-    username       = "Terraform access token"
+    username       = "tomrenard69@gmail.com"
     password       = "glpat-4sRgzkxWmdr1xhGx8j2q"
     }
   
