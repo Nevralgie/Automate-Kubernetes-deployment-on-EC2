7ffab89b5122871c67a6983601c8637aaab9c0f8
Tom Renard <tomrenard69@gmail.com>
2024-04-30 09:39:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,14 @@ resource "aws_internet_gateway" "gw" {
     Name = "main_IGW"
   }
 }
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.main_vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+
 resource "aws_subnet" "sub_1_ec2_lb" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
