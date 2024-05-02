865a9ba2b1f75ac4880ff9b2fbc97d6fd8b7ce3e
Tom Renard <tomrenard69@gmail.com>
2024-05-02 15:22:06 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,22 +8,28 @@ terraform {
 
 
 
-  backend "http" {
-    address        = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME"
-    lock_address   = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
-    unlock_address = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
-    username       = "Nevii"
-    password       = "glpat-xzX_t6eZy-nhbHaG4DzQ"
-    lock_method    = "POST"
-    unlock_method  = "DELETE"
-    retry_wait_min = 5
-    }
+  # backend "http" {
+  #   address        = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME"
+  #   lock_address   = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
+  #   unlock_address = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
+  #   username       = "Nevii"
+  #   password       = "glpat-xzX_t6eZy-nhbHaG4DzQ"
+  #   lock_method    = "POST"
+  #   unlock_method  = "DELETE"
+  #   retry_wait_min = 5
+  #   }
   
 }
 provider "aws" {
-  region = "eu-west-3" 
+  region = "eu-west-3"
+  access_key = "AKIAYM4CN735ZH4AQ5DO"
+  secret_key = "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"
 }
 
+
+data "aws_vpc" "target_peering_vpc" {
+  id = "vpc-0f4d614772ca8d3f0"
+}
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
@@ -122,41 +128,41 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 }
 
 
-resource "aws_security_group" "rds_security_group" {
-  name        = "rds_security_group"
-  description = "Allow inbound traffic from EC2 security group"
-  vpc_id      = aws_vpc.main_vpc.id
-}
+# resource "aws_security_group" "rds_security_group" {
+#   name        = "rds_security_group"
+#   description = "Allow inbound traffic from EC2 security group"
+#   vpc_id      = aws_vpc.main_vpc.id
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_mysql" {
-  security_group_id = aws_security_group.rds_security_group.id
-  referenced_security_group_id = aws_security_group.allow_ssh_http.id
-  from_port         = 3306
-  ip_protocol       = "tcp"
-  to_port           = 3306
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_mysql" {
+#   security_group_id = aws_security_group.rds_security_group.id
+#   referenced_security_group_id = aws_security_group.allow_ssh_http.id
+#   from_port         = 3306
+#   ip_protocol       = "tcp"
+#   to_port           = 3306
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_out" {
-  security_group_id = aws_security_group.rds_security_group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_out" {
+#   security_group_id = aws_security_group.rds_security_group.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
 
-resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  db_name              = "db_app"
-  identifier           = "devopsdb-app"
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = var.environment == "Prod" ? "db.t3.large" : "db.t3.micro"
-  username             = "admin"
-  password             = "vAdmintestv"
-  db_subnet_group_name = aws_db_subnet_group.default.name
-  multi_az             = false
-  vpc_security_group_ids = [aws_security_group.rds_security_group.id]
-  skip_final_snapshot  = var.environment == "Prod" ? false : true
-  final_snapshot_identifier = var.environment == "Prod" ? "Db_snapshot" : null
-}
+# resource "aws_db_instance" "default" {
+#   allocated_storage    = 20
+#   db_name              = "db_app"
+#   identifier           = "devopsdb-app"
+#   engine               = "mysql"
+#   engine_version       = "8.0"
+#   instance_class       = var.environment == "Prod" ? "db.t3.large" : "db.t3.micro"
+#   username             = "admin"
+#   password             = "vAdmintestv"
+#   db_subnet_group_name = aws_db_subnet_group.default.name
+#   multi_az             = false
+#   vpc_security_group_ids = [aws_security_group.rds_security_group.id]
+#   skip_final_snapshot  = var.environment == "Prod" ? false : true
+#   final_snapshot_identifier = var.environment == "Prod" ? "Db_snapshot" : null
+# }
 
 resource "aws_instance" "control_plane" {
   count         = var.control_plane_instance_number
@@ -192,27 +198,45 @@ resource "aws_instance" "workers" {
   }
 }
 
+resource "aws_vpc_peering_connection" "vpc_gitlab_runner" {
+  peer_vpc_id   = data.aws_vpc.target_peering_vpc.id
+  vpc_id        = aws_vpc.main_vpc.id
+
+  tags = {
+    Side = "Requester"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_gitlab_runner.id
+  auto_accept               = true
+
+  tags = {
+    Side = "Accepter"
+  }
+}
+
 # Outputs for RDS instance
-output "database_endpoint" {
-  description = "The endpoint of the database"
-  value       = aws_db_instance.default.endpoint
-}
+# output "database_endpoint" {
+#   description = "The endpoint of the database"
+#   value       = aws_db_instance.default.endpoint
+# }
 
-output "database_username" {
-  description = "The username of the database"
-  value       = aws_db_instance.default.username
-}
+# output "database_username" {
+#   description = "The username of the database"
+#   value       = aws_db_instance.default.username
+# }
 
-output "database_password" {
-  description = "The password of the database"
-  value       = aws_db_instance.default.password
-  sensitive = true
-}
+# output "database_password" {
+#   description = "The password of the database"
+#   value       = aws_db_instance.default.password
+#   sensitive = true
+# }
 
-output "database_name" {
-  description = "The name of the database"
-  value       = aws_db_instance.default.db_name
-}
+# output "database_name" {
+#   description = "The name of the database"
+#   value       = aws_db_instance.default.db_name
+# }
 
 output "ctrl_instance_name" {
   value = aws_instance.workers[*].tags
@@ -220,4 +244,31 @@ output "ctrl_instance_name" {
 
 output "wrks_instance_name" {
   value = aws_instance.control_plane[*].tags
+}
+
+output "private_ip" {
+  value = aws_instance.control_plane[*].private_ip
+}
+
+# Create a route for the second VPC's CIDR block in the first VPC's route table
+resource "aws_route" "main_vpc_route_to_k8s" {
+  route_table_id            = "rtb-0cb9914b16f6618ed"
+  destination_cidr_block    = "10.0.0.0/16" # Replace with the other VPC's CIDR block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_gitlab_runner.id
+}
+
+
+
+# Create a route for the first VPC's CIDR block in the second VPC's route table
+resource "aws_route" "vpc2_route" {
+  route_table_id            = aws_vpc.main_vpc.main_route_table_id
+  destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_gitlab_runner.id
+}
+
+# Associate the route tables with the subnets in the VPCs
+# Replace the subnet IDs with the actual IDs of the subnets in the VPCs
+
+data "aws_subnet" "gitlab_runner_sub" {
+  id = "subnet-0eea324505858e7f7"
 }
