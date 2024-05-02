3ffa9f139b1981abdc0bdf1a189df57d5fb3d3c0
Tom Renard <tomrenard69@gmail.com>
2024-05-02 15:23:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,16 +8,16 @@ terraform {
 
 
 
-  # backend "http" {
-  #   address        = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME"
-  #   lock_address   = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
-  #   unlock_address = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
-  #   username       = "Nevii"
-  #   password       = "glpat-xzX_t6eZy-nhbHaG4DzQ"
-  #   lock_method    = "POST"
-  #   unlock_method  = "DELETE"
-  #   retry_wait_min = 5
-  #   }
+  backend "http" {
+    address        = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME"
+    lock_address   = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
+    username       = "Nevii"
+    password       = "glpat-xzX_t6eZy-nhbHaG4DzQ"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    retry_wait_min = 5
+    }
   
 }
 provider "aws" {
