71d38137a0fddf05720b6af6b702d55fc05871fa
Tom Renard <tomrenard69@gmail.com>
2024-05-28 11:53:38 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -120,6 +120,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 80
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 6443
+  ip_protocol       = "tcp"
+  to_port           = 6443
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_ssh_http.id
   cidr_ipv4         = "0.0.0.0/0"
