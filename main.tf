75a2334ec7624470d46b57eaa48e817c652ea42c
Tom Renard <tomrenard69@gmail.com>
2024-07-12 08:43:29 +0000

    Update 2 files
    - /main.tf
    - /.gitlab-ci.yml

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
     lock_address   = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
     unlock_address = "https://gitlab.com/api/v4/projects/57372801/terraform/state/$TF_STATE_NAME/lock"
     username       = "Nevii"
-    password       = "$CI_JOB_TOKEN"
+    password       = "glpat-PFA64cyNktWYFSUHTSUT"
     lock_method    = "POST"
     unlock_method  = "DELETE"
     retry_wait_min = 5
