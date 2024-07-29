e9dcba504e9767196ea48d0ed617e97a5ac8ef22
Tom Renard <tomrenard69@gmail.com>
2024-07-29 09:36:30 +0000

    Update file app.py

--- app/app.py
+++ app/app.py
@@ -107,4 +107,4 @@ def index():
     return render_template('stock_analysis.html', stock_data=stock_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
