521b10b24cf2a572a9a97a6bea32716df785b9e5
Tom Renard <tomrenard69@gmail.com>
2024-08-15 20:18:34 +0000

    Update 6 files
    - /.gitlab-ci.yml
    - /Terraform_Infra/outputs.tf
    - /Ansible/playbooks/packages.yml
    - /Ansible/playbooks/app.yml
    - /Ansible/playbooks/check_resources.yml
    - /Ansible/main_playbook.yml

--- Terraform_Infra/outputs.tf
+++ Terraform_Infra/outputs.tf
@@ -2,14 +2,22 @@ output "wrks_instance_name" {
   value = aws_instance.workers[*].tags
 }
 
+output "wrks_instance_pub_ip" {
+  value = aws_instance.workers[*].public_ip
+}
+
+output "wrks_private_ip" {
+  value = aws_instance.workers[*].private_ip
+}
+
 output "ctrl_instance_name" {
   value = aws_instance.control_plane[*].tags
 }
 
-output "private_ip" {
+output "ctl_private_ip" {
   value = aws_instance.control_plane[*].private_ip
 }
 
-output "public_ip" {
+output "ctl_public_ip" {
   value = aws_instance.control_plane[*].public_ip
 }
