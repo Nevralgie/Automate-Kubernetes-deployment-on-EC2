289dcb0b2158d031a6fee53fcb738f039143c666
Tom Renard <tomrenard69@gmail.com>
2024-08-15 17:06:09 +0000

    Update 5 files
    - /Ansible/playbooks/kube-dependencies_master.yml
    - /Ansible/playbooks/kube-dependencies_workers.yml
    - /Ansible/playbooks/master.yml
    - /Ansible/inventory.aws_ec2.yaml
    - /Terraform_Infra/nsg.tf

--- Terraform_Infra/nsg.tf
+++ Terraform_Infra/nsg.tf
@@ -21,6 +21,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ctlplane_from_gitlab_r
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "core_dns_from_workers" {
+  security_group_id = aws_security_group.kubernetes_controlplane.id
+  referenced_security_group_id = aws_security_group.kubernetes_workers.id
+  from_port         = 53
+  ip_protocol       = "tcp"
+  to_port           = 53
+}
+
 resource "aws_vpc_security_group_ingress_rule" "calico_networking_bgp_ctl" {
   security_group_id = aws_security_group.kubernetes_controlplane.id
   referenced_security_group_id = aws_security_group.kubernetes_workers.id
@@ -65,6 +73,14 @@ resource "aws_security_group" "kubernetes_workers" {
   vpc_id      = aws_vpc.main_vpc.id 
 }
 
+resource "aws_vpc_security_group_ingress_rule" "core_dns_from_ctlplane" {
+  security_group_id = aws_security_group.kubernetes_workers.id
+  referenced_security_group_id = aws_security_group.kubernetes_controlplane.id
+  from_port         = 53
+  ip_protocol       = "tcp"
+  to_port           = 53
+}
+
 resource "aws_vpc_security_group_ingress_rule" "calico_networking_bgp_wks" {
   security_group_id = aws_security_group.kubernetes_workers.id
   referenced_security_group_id = aws_security_group.kubernetes_workers.id
@@ -80,6 +96,7 @@ resource "aws_vpc_security_group_ingress_rule" "calico_networking_bgp_ctltowks" 
   ip_protocol       = "tcp"
   to_port           = 179
 }
+
 
 resource "aws_vpc_security_group_ingress_rule" "kubelet_api" {
   security_group_id = aws_security_group.kubernetes_workers.id
